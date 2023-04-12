7f271353b1e082e797fbe067fd487ee11cfcf76e
SidWorks01 <86377449+SidWorks01@users.noreply.github.com>
2023-04-12 22:55:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu').addEventListener('click', function(e) {
+    document.querySelector('#navbar').classList.add('active');
+})
+
+document.querySelector('.close').addEventListener('click', function(e) {
+    document.querySelector('#navbar').classList.remove('active');
+})
